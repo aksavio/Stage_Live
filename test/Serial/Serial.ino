cde4eac862d65252ab205c90ce62f98b879b3e8a
aksavio <6294426+aksavio@users.noreply.github.com>
2025-09-11 00:07:31 +0530

    added arduino code

---
+++ test/Serial/Serial.ino
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+#define HANDSHAKE_REQUEST "ESP_READY"
+#define HANDSHAKE_RESPONSE "PI_ACK"
+
+bool handshakeDone = false;
+
+void setup() {
+  Serial.begin(115200);
+  delay(2000); // wait for Serial to come up
+  Serial.println(HANDSHAKE_REQUEST); // tell Pi that ESP is ready
+}
+
+void loop() {
+  if (!handshakeDone && Serial.available()) {
+    String msg = Serial.readStringUntil('\n');
+    msg.trim();
+    if (msg == HANDSHAKE_RESPONSE) {
+      Serial.println("Handshake successful!");
+      handshakeDone = true;
+    }
+  }
+
+  if (handshakeDone) {
+    Serial.println("Hello from ESP!");
+    delay(2000);
+  }
+}
+
