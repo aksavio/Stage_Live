3704c5fa1a7b99d8a1fed453d10947bbe9bbe682
aksavio <6294426+aksavio@users.noreply.github.com>
2025-11-29 14:34:08 +0530

    esp not connecting im checking

--- Stage_Live_Performer/core.py
+++ Stage_Live_Performer/core.py
@@ -8,26 +8,26 @@ import time
 HANDSHAKE_REQUEST = "ESP_READY"
 HANDSHAKE_RESPONSE = "PI_ACK"
 
-# def find_esp():
-#     ports = serial.tools.list_ports.comports()
-#     for port in ports:
-#         try:
-#             print(f"Trying {port.device}...")
-#             ser = serial.Serial(port.device, 115200, timeout=2)
-#             time.sleep(2)  # wait for ESP reset on new serial connection
+def find_esp():
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        try:
+            print(f"Trying {port.device}...")
+            ser = serial.Serial(port.device, 115200, timeout=2)
+            time.sleep(2)  # wait for ESP reset on new serial connection
 
-#             # give ESP time to send message
-#             line = ser.readline().decode("utf-8").strip()
-#             if line == HANDSHAKE_REQUEST:
-#                 print(f"✅ Found ESP on {port.device}")
-#                 ser.write((HANDSHAKE_RESPONSE + "\n").encode("utf-8"))
-#                 return ser  # return the open serial connection
-#             else:
-#                 print(f"No handshake message from {port.device} (got: '{line}')")
-#                 ser.close()
-#         except Exception as e:
-#             print(f"Error with {port.device}: {e}")
-#     return port.device
+            # give ESP time to send message
+            line = ser.readline().decode("utf-8").strip()
+            if line == HANDSHAKE_REQUEST:
+                print(f"✅ Found ESP on {port.device}")
+                ser.write((HANDSHAKE_RESPONSE + "\n").encode("utf-8"))
+                return ser  # return the open serial connection
+            else:
+                print(f"No handshake message from {port.device} (got: '{line}')")
+                ser.close()
+        except Exception as e:
+            print(f"Error with {port.device}: {e}")
+    return port.device
 
 # if __name__ == "__main__":
 #     esp_serial = find_esp()
